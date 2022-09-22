e64240bfc30829f12d9a4e30f5c836aca9a459f0
Mobolaji Kupoluyi <bjkuppy@gmail.com>
2022-09-22 08:49:21 +0100

    refactor: refactored some codes in the basic testing file

--- src/main/java/basics/BasicsTesting.java
+++ src/main/java/basics/BasicsTesting.java
@@ -4,20 +4,26 @@ import groovy.json.JsonParser;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class BasicsTesting {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         // given() - all the input details
         // when() - submit the API -resource,http method
         // then() - validate the response
+        // to supply the body with a payload.json file
+        // convert the content of the file to string
         baseURI = "https://rahulshettyacademy.com";
-
         // ADD new place
-        String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body(PayLoad.addPlace()).when().post("/maps/api/place/add/json").then().assertThat().statusCode(200)
-                .body("scope", equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+        String response = null;
+            response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                    .body(PayLoad.addPlace()).when().post("/maps/api/place/add/json").then().assertThat().statusCode(200)
+                    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
 
         // parsing the string into Json to get the placeID by the path
         String placeID = ReUsableMethods.convertRawToJson(response).getString("place_id");
@@ -43,6 +49,10 @@ public class BasicsTesting {
         System.out.println();
         Assert.assertEquals(actualAddress,inputedNewAddress);
 
+    }
+
+    public static String generateStringFromResource(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 
 
